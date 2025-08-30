d52bde26319cf31463476042958abd4238e5f2c4
manyfaces-coder <semenovoleg99@yandex.ru>
2025-08-30 21:35:57 +0300

    config берет данные для подключения из .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_FILE = BASE_DIR / "project-root" / ".env"
 
 # Параметры для запуска приложения
 class RunConfig(BaseModel):
@@ -20,6 +23,13 @@ class DatabaseConfig(BaseModel):
     max_overflow: int = 10
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        case_sensitive=False, # регистр неважен
+        env_nested_delimiter="__", # разделитель
+        env_prefix="APP_CONFIG__",
+        extra="ignore",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
